1c0faecf3aedf54f516967cb2cb3d5df3feae435
Edouard-LARROCHE <edouard-larroche_student2021@wilder.school>
2022-02-08 23:43:17 +0100

    drop & down menu component

--- front/src/components/home/NavBar.jsx
+++ front/src/components/home/NavBar.jsx
@@ -3,49 +3,53 @@ import { TextField } from '@material-ui/core';
 import SearchIcon from '@material-ui/icons/Search';
 import PersonIcon from '@material-ui/icons/Person';
 import { Link } from 'react-router-dom';
+import DropDown from '../DropDown';
 
 const NavBar = () => {
   const [name, setName] = useState('');
 
   return (
-    <div className='all-nav'>
-      <div className='nav-bar'>
-        <ul>
-          <li className='underline'>
-            <Link to='/'>
-              <p>La Maison Morin</p>
-            </Link>
-          </li>
-          <li className='underline'>
-            <Link to='/'>
-              <p>Notre boutique</p>
-            </Link>
-          </li>
-          <li className='underline'>
-            <Link to='/contact'>
-              <p>Nous contacter</p>
-            </Link>
-          </li>
-        </ul>
+    <>
+      <div className='all-nav'>
+        <div className='nav-bar'>
+          <ul>
+            <li className='underline'>
+              <Link to='/'>
+                <p>La Maison Morin</p>
+              </Link>
+            </li>
+            <li className='underline'>
+              <Link to='/'>
+                <p>Notre boutique</p>
+              </Link>
+            </li>
+            <li className='underline'>
+              <Link to='/contact'>
+                <p>Nous contacter</p>
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div className='logo-home'>
+          <Link to='/'>
+            <img src='/logo/logo-morin.png' alt='logo-morin' />
+          </Link>
+          <h2>MAISON MORIN</h2>
+        </div>
+        <div className='other-nav'>
+          <ul>
+            <li>
+              <TextField className='text-field' type='text' onChange={(e) => setName(e.target.value)} placeholder='Rechercher' value={name} />
+              <SearchIcon style={{ cursor: 'pointer' }} />
+            </li>
+            <li>
+              <PersonIcon style={{ cursor: 'pointer' }} />
+            </li>
+          </ul>
+        </div>
       </div>
-      <div className='logo-home'>
-        <Link to='/'>
-          <img src='/logo/logo-morin.png' alt='logo-morin' />
-        </Link>
-        <h2>MAISON MORIN</h2>
-      </div>
-      <div className='other-nav'>
-        <ul>
-          <li>
-            <TextField className='text-field' type='text' onChange={(e) => setName(e.target.value)} placeholder='Rechercher' value={name} />
-            <SearchIcon style={{ cursor: 'pointer' }} />
-          </li>
-          <li>
-            <PersonIcon style={{ cursor: 'pointer' }} />
-          </li>
-        </ul>
-      </div>
-    </div>
+      <DropDown />
+    </>
   );
 };
 
