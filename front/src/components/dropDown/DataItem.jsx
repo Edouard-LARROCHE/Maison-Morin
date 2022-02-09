e65202b0f2441654fe0c553efa9b786dcf509dd5
Edouard-LARROCHE <edouard-larroche_student2021@wilder.school>
2022-02-09 17:03:04 +0100

    email-js

--- front/src/components/dropDown/DataItem.jsx
+++ front/src/components/dropDown/DataItem.jsx
@@ -1,33 +1,31 @@
 import React from 'react';
 
-const data = {
-  nav: [
-    {
-      mainTitle: 'morin1',
-      subItem: [{ title: 'item1' }, { title: 'item2' }],
-    },
-    {
-      mainTitle: 'morin2',
-      subItem: [{ title: 'item1' }, { title: 'item2' }],
-    },
-    {
-      mainTitle: 'morin3',
-      subItem: [{ title: 'item1' }, { title: 'item2' }],
-    },
-    {
-      mainTitle: 'morin4',
-      subItem: [{ title: 'item1' }, { title: 'item2' }],
-    },
-  ],
-};
+const data = [
+  {
+    mainTitle: 'morin1',
+    subItem: [{ title: 'item1' }, { title: 'item2' }],
+  },
+  {
+    mainTitle: 'morin2',
+    subItem: [{ title: 'item1' }, { title: 'item2' }],
+  },
+  {
+    mainTitle: 'morin3',
+    subItem: [{ title: 'item1' }, { title: 'item2' }],
+  },
+  {
+    mainTitle: 'morin4',
+    subItem: [{ title: 'item1' }, { title: 'item2' }],
+  },
+];
 
 const DataItem = () => {
   return (
     <div className='header-nav'>
       <div className='content-nav'>
         <ul>
-          {data.nav.map((item) => {
-            return;
+          {data.map((item) => {
+            return <div key={item}> {} </div>;
           })}
         </ul>
       </div>
