3ec02ae2655227e7facfc4a96db181b5e1cd3f94
Edouard-LARROCHE <edouard-larroche_student2021@wilder.school>
2022-02-14 12:30:08 +0100

    login-modal

--- front/src/components/LoginModal/indexModal.jsx
+++ front/src/components/LoginModal/indexModal.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 import PersonIcon from '@material-ui/icons/Person';
 import { TextField } from '@material-ui/core';
 import useModal from './useModal';
@@ -10,41 +11,63 @@ const IndexModal = () => {
   const [name, setName] = useState('');
   const [password, setPassword] = useState('');
 
-  const logErr = () => {
-    const err = document.querySelector('.err-login');
-    err.innerHTML = 'Nom ou mot de passe invalide';
-    if (name && password) {
-      err.style.display = 'none';
-    } else {
-      err.style.display = 'block';
-    }
-  };
+  // const logErr = () => {
+  //   const err = document.querySelector('.err-login');
+  //   err.innerHTML = 'Nom ou mot de passe invalide';
+  //   if (name && password) {
+  //     err.style.display = 'none';
+  //   } else {
+  //     err.style.display = 'block';
+  //   }
+  // };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    logErr();
-    setName('');
-    setPassword('');
+    const nameError = document.querySelector('.name-err');
+    const passwordError = document.querySelector('.pass-err');
+
+    axios({
+      method: 'get',
+      url: 'http://localhost:5500/login',
+      withCredentials: true,
+      data: {
+        name,
+        password,
+      },
+    })
+      .then((res) => {
+        console.log(res);
+        if (res.data.errors) {
+          nameError.innerHTML = res.data.errors.name;
+          passwordError.innerHTML = res.data.errors.password;
+        } else {
+          window.location = '/contact';
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
 
   return (
     <div className='login-component'>
       <PersonIcon className='modal-toggle' onClick={toggleLoginForm} style={{ cursor: 'pointer', color: '#012f6b' }} />
       <Modal isShowing={isLoginFormShowed} hide={toggleLoginForm}>
-        <form>
+        <form onSubmit={handleSubmit}>
           <TextField onChange={(e) => setName(e.target.value)} value={name} type='text' placeholder='Nom' style={{ paddingBottom: '1rem' }} />
 
           <TextField
             onChange={(e) => setPassword(e.target.value)}
             value={password}
-            type='text'
+            type='password'
             placeholder='Mot de passe'
             style={{ paddingBottom: '1rem' }}
           />
-          <div className='err-login' />
-          <div className='button' onClick={handleSubmit}>
+          <div className='name-err' />
+          <div className='pass-err' />
+          <button className='button' type='submit'>
             <p>Connexion</p>
-          </div>
+          </button>
         </form>
       </Modal>
     </div>
