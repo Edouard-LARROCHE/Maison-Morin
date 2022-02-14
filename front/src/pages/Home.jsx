56fd057518fda566fd78452670fba07256a65d04
Edouard-LARROCHE <edouard-larroche_student2021@wilder.school>
2022-02-14 23:32:09 +0100

    component header home

--- front/src/pages/Home.jsx
+++ front/src/pages/Home.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import MenuDrop from '../components/dropDown/MenuDrop';
-import Footer from '../components/home/Footer';
 import NavBar from '../components/home/NavBar';
+import MenuDrop from '../components/dropDown/MenuDrop';
+import Header from '../components/home/Header';
+import Footer from '../components/home/Footer';
 
 const Home = () => {
   return (
     <div>
       <NavBar />
       <MenuDrop />
+      <Header />
       <Footer />
     </div>
   );
