e2ea56ae8e1f8b3ee88cee74d532b7006839f8f9
Exemple Nom Uti <email@email.email>
2022-02-14 19:13:44 +0100

    fix base newsletter

--- front/src/components/newsletter/NewsLetter.jsx
+++ front/src/components/newsletter/NewsLetter.jsx
@@ -2,22 +2,18 @@ import React from 'react';
 import { TextField } from '@material-ui/core';
 
 const NewsLetter = () => {
-    return (
+    return (<>
+        <div className="line"></div>
         <div className="banner-nl">
-            <div className="content-nl">
-                <div className="bg-logo-nl">
                     <div className="logo-nl">
                         <img src='/assets/morin_solo_blanc.png' alt='logo-morin' height='120em' width='120em' />
                     </div>
-                </div>
-                <div className="text-nl">
                     <div className="titre-nl">
                         <h1>News Letter</h1>
                     </div>
                     <div className="text-info-nl">
                         <h3>Soyez les premiers à être informés de nos actualités !</h3>
                     </div>
-                </div>
                 <div className="email-nl">
                     <TextField
                         className='textfield-email'
@@ -29,10 +25,10 @@ const NewsLetter = () => {
                     />
                 </div>
                 <div className="button-nl">
-                    <p>Je m'inscris!</p>
+                    <p>Je m'inscris</p>
                 </div>
-            </div>
         </div>
+    </>
     );
 };
 
