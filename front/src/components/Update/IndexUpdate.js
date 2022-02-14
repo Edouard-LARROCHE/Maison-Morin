a7ef344f35b11b420b20091f73153ba842426bd4
Edouard-LARROCHE <edouard-larroche_student2021@wilder.school>
2022-02-14 16:47:35 +0100

    back button

--- front/src/components/Update/IndexUpdate.js
+++ front/src/components/Update/IndexUpdate.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const IndexUpdate = () => {
   return (
     <div>
       <h1>Crud update data</h1>
+      <Link to='/'>
+        <button>Retour</button>
+      </Link>
     </div>
   );
 };
