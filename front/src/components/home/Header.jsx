fb2a7b30912122441dfa9a6c11c099ba98a141f3
Edouard-LARROCHE <edouard-larroche_student2021@wilder.school>
2022-02-15 15:21:29 +0100

    component header home

--- front/src/components/home/Header.jsx
+++ front/src/components/home/Header.jsx
@@ -21,7 +21,19 @@ const Header = () => {
           <p>Nous changeons régulièrement la carte pour renouveler continuellement vos expériences et vous surprendre.</p>
         </div>
       </div>
-      <div className='picture-head'></div>
+      <div className='picture-head'>
+        <div className='picture-1'>
+          <img src='/assets/cake-3.png' alt='cake3' style={{ width: '17rem' }} />
+        </div>
+        <div className='picture-bottom'>
+          <div className='picture-2'>
+            <img src='/assets/cake9.png' alt='cake9' style={{ width: '17rem' }} />
+          </div>
+          <div className='picture-3'>
+            <img src='/assets/cake-10.png' alt='cake10' style={{ width: '17rem' }} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
