13789bdbe99e03842aa58f0b4d40dda7fc69cd97
Exemple Nom Uti <email@email.email>
2022-02-17 16:41:16 +0100

    rename middle -> second

---
+++ front/src/components/home/Second.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Second = () => {
+  return (<>
+      <div className="banner-second">
+        <div className="title-second">
+        </div>
+        <div className="support-second">
+        </div>
+      </div>
+      <div className="photos">
+        <img className='img-un' src='/assets/cake5.png' alt="" />
+        
+        <img className='img-deux' src='/assets/cake9.png' alt="" />
+        <img className='img-trois' src='/assets/index9.jpeg' alt="" />
+        <img className='img-quatre' src='/assets/index10.jpeg' alt="" />
+        <img className='img-cinq' src='/assets/cake4.png' alt="" />
+        <img className='img-six' src='/assets/index11.jpeg' alt="" />
+        <img className='img-sept' src='/assets/cake.png' alt="" />
+        <img className='img-huit' src='/assets/index8.jpeg' alt="" /><a className='test' href="https://www.facebook.com">Test</a>
+        {/* <img className='img-neuf' src='/assets/cake.png' alt="" /> */}
+      </div>
+  </>);
+};
+
+export default Second;
