080ee86358922dcdfd201d2ea53e690fee47d127
Edouard-LARROCHE <edouard-larroche_student2021@wilder.school>
2022-02-17 21:45:25 +0100

    carousel home

--- front/src/components/Carousel.jsx
+++ front/src/components/Carousel.jsx
@@ -3,79 +3,63 @@ import { Carousel } from 'react-carousel-minimal';
 function Carou() {
   const data = [
     {
-      image: '/assets/cake5.png',
-      caption: 'San Francisco',
+      image: '/assets/cake4.png',
     },
     {
-      image: 'https://cdn.britannica.com/s:800x450,c:crop/35/204435-138-2F2B745A/Time-lapse-hyper-lapse-Isle-Skye-Scotland.jpg',
-      caption: 'Scotland',
+      image: '/assets/cake2.png',
     },
     {
-      image: 'https://static2.tripoto.com/media/filter/tst/img/735873/TripDocument/1537686560_1537686557954.jpg',
-      caption: 'Darjeeling',
+      image: '/assets/index.jpeg',
     },
     {
       image:
         'https://upload.wikimedia.org/wikipedia/commons/thumb/1/16/Palace_of_Fine_Arts_%2816794p%29.jpg/1200px-Palace_of_Fine_Arts_%2816794p%29.jpg',
-      caption: 'San Francisco',
     },
     {
       image: 'https://i.natgeofe.com/n/f7732389-a045-402c-bf39-cb4eda39e786/scotland_travel_4x3.jpg',
-      caption: 'Scotland',
     },
     {
       image: 'https://www.tusktravel.com/blog/wp-content/uploads/2020/07/Best-Time-to-Visit-Darjeeling-for-Honeymoon.jpg',
-      caption: 'Darjeeling',
     },
     {
       image: 'https://www.omm.com/~/media/images/site/locations/san_francisco_780x520px.ashx',
-      caption: 'San Francisco',
     },
     {
       image:
         'https://images.ctfassets.net/bth3mlrehms2/6Ypj2Qd3m3jQk6ygmpsNAM/61d2f8cb9f939beed918971b9bc59bcd/Scotland.jpg?w=750&h=422&fl=progressive&q=50&fm=jpg',
-      caption: 'Scotland',
     },
     {
       image: 'https://www.oyorooms.com/travel-guide/wp-content/uploads/2019/02/summer-7.jpg',
-      caption: 'Darjeeling',
     },
   ];
 
-  const captionStyle = {
-    fontSize: '2em',
-    fontWeight: 'bold',
-  };
   const slideNumberStyle = {
-    fontSize: '20px',
-    fontWeight: 'bold',
+    fontSize: '15px',
+    color: '#012f6b',
   };
   return (
-    <div className='App'>
-      <div style={{ textAlign: 'center' }}>
-        <h2>React Carousel Minimal</h2>
-        <p>Easy to use, responsive and customizable carousel component for React Projects.</p>
+    <div className='carousel'>
+      <div>
         <div
           style={{
             padding: '0 20px',
           }}>
           <Carousel
             data={data}
-            time={2000}
+            time={7000}
             width='850px'
-            height='500px'
-            captionStyle={captionStyle}
+            height='400px'
             radius='10px'
             slideNumber={true}
             slideNumberStyle={slideNumberStyle}
             captionPosition='bottom'
-            automatic={false}
-            dots={true}
-            pauseIconColor='white'
-            pauseIconSize='40px'
-            slideBackgroundColor='darkgrey'
+            automatic={true}
+            dots={false}
+            pauseIconColor='#012f6b'
+            pauseIconSize='20px'
+            slideBackgroundColor='grey'
             slideImageFit='cover'
-            thumbnails={true}
+            thumbnails={false}
             thumbnailWidth='100px'
             style={{
               textAlign: 'center',
