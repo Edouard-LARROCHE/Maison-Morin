1df34d8a75b605a1547cf9240355d03de43be113
Edouard-LARROCHE <edouard-larroche_student2021@wilder.school>
2022-02-17 17:18:00 +0100

    carousel

--- front/src/pages/Shop.jsx
+++ front/src/pages/Shop.jsx
@@ -3,6 +3,7 @@ import NavBar from '../components/home/NavBar';
 // import Map from '../components/Shop/Map';
 import HeadBand from '../components/HeadBand';
 import Footer from '../components/home/Footer';
+// import Carou from '../components/Carousel';
 
 const Shop = () => {
   return (
@@ -10,6 +11,7 @@ const Shop = () => {
       <NavBar />
       <HeadBand />
       {/* <Map /> */}
+      {/* <Carou /> */}
       <Footer />
     </div>
   );
