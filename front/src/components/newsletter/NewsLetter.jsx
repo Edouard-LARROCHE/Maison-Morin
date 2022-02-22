1bbe695d3420ed85b430b56b96fe183223468d01
Edouard-LARROCHE <edouard-larroche_student2021@wilder.school>
2022-02-22 23:09:08 +0100

    indentation...

--- front/src/components/newsletter/NewsLetter.jsx
+++ front/src/components/newsletter/NewsLetter.jsx
@@ -2,31 +2,32 @@ import React from 'react';
 import { TextField } from '@material-ui/core';
 
 const NewsLetter = () => {
-    return (<>
-        <div className="line"></div>
-        <div className="banner-nl">
-                    <div className="titre-nl">
-                        <h1>Newsletter</h1>
-                    </div>
-                    <div className="text-info-nl">
-                        <h3>Soyez les premiers à être informés de nos actualités !</h3>
-                    </div>
-                <div className="email-nl">
-                    <TextField
-                        className='textfield-email'
-                        style={{ paddingBottom: '2rem' }}
-                        type='text'
-                        id='email'
-                        name='email'
-                        placeholder='Votre adresse mail'
-                    />
-                </div>
-                <div className="btn-nl">
-                    <p>Je m'inscris</p>
-                </div>
+  return (
+    <>
+      <div className='line'></div>
+      <div className='banner-nl'>
+        <div className='titre-nl'>
+          <h1>Newsletter</h1>
         </div>
+        <div className='text-info-nl'>
+          <h3>Soyez les premiers à être informés de nos actualités !</h3>
+        </div>
+        <div className='email-nl'>
+          <TextField
+            className='textfield-email'
+            style={{ paddingBottom: '2rem' }}
+            type='text'
+            id='email'
+            name='email'
+            placeholder='Votre adresse mail'
+          />
+        </div>
+        <div className='btn-nl'>
+          <p>Je m'inscris</p>
+        </div>
+      </div>
     </>
-    );
+  );
 };
 
 export default NewsLetter;
