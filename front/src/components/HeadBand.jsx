73938734ffbb014022969c2e1998731efe4cd4e2
Edouard-LARROCHE <edouard-larroche_student2021@wilder.school>
2022-02-23 16:30:22 +0100

    head-band down

--- front/src/components/HeadBand.jsx
+++ front/src/components/HeadBand.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 
 const HeadBand = () => {
   return (
-    <div>
-      <div className='vertical-line-global' />
+    <div className='line-global'>
+      <div className='line-horizontal-left' />
+      <div className='line-horizontal-right' />
+      <div className='line-horizontal-left-deg' />
+      <div className='line-horizontal-right-deg' />
     </div>
   );
 };
