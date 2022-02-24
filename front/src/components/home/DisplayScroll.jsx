214d844ebea22653ff148024d2755d33fb18d9bd
Edouard-LARROCHE <edouard-larroche_student2021@wilder.school>
2022-02-24 14:52:57 +0100

    display-first

--- front/src/components/home/DisplayScroll.jsx
+++ front/src/components/home/DisplayScroll.jsx
@@ -1,27 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const DisplayScroll = () => {
+  const [headText, setHeadText] = useState('head-text-before');
+  const [picture, setPicture] = useState('picture-first-before');
+  const [title, setTitle] = useState('title-header-before');
+  const [text, setText] = useState('text-content-before');
+  const [linkTo, setLinkTo] = useState('link-to-first-before');
+
+  const handleScroll = () => {
+    if (window.scrollY >= 400) {
+      setHeadText('head-text');
+      setPicture('picture-first');
+      setTimeout(() => {
+        setTitle('title-header');
+      }, 500);
+    }
+    if (window.scrollY >= 800) {
+      setText('text-content');
+    }
+    if (window.scrollY >= 1100) {
+      setLinkTo('link-to-first');
+    }
+  };
+
+  window.addEventListener('scroll', handleScroll);
+
   return (
     <div className='display-first'>
-      <div className='picture-first'>
+      <div className={picture}>
         <img src='/assets/macaron.png' alt='picute1' />
       </div>
       <div className='text-first'>
-        <div className=''>Une Passion au Quotidien </div>
-        <div className='title-header'>
-          <h1>TRAITEUR TRADITIONNEL</h1>
+        <div className={headText}>Une Passion au Quotidien </div>
+        <div className={title}>
+          <h1>PÂTISSERIE</h1>
+          <div className='V-position'>
+            <img className='V' src='/logo/V.png' alt='V' style={{ width: '2rem' }} />
+          </div>
         </div>
-        <div className='vertical-line' />
-        <div className='text-content'>
-          <p>Notre boutique traîteur en Seine et Marne est spécialisée en cuisine traditionnelle française.</p>
-          <p>Nous vous proposons de nombreux plats régionaux et plats du terroir variés. Nous utilisons des produits frais et de saison.</p>
+        <div className={text}>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos amet corrupti molestias accusamus eligendi praesentium similique architecto,
+            porro, eius perspiciatis libero tempore deleniti ad. Odio placeat rem veritatis totam excepturi!
+          </p>
+          <br />
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, dolores accusantium? Nostrum sit quasi labore!</p>
         </div>
-        <div className='vertical-line' />
       </div>
-      <div className=''>
+      <div className={linkTo}>
         <Link to='/'>
-          <p>DECOUVRIR LA MAISON »</p>
+          <p>NOS RÉALISATIONS »</p>
         </Link>
       </div>
     </div>
--- front/src/components/home/Header.jsx
+++ front/src/components/home/Header.jsx
@@ -18,7 +18,7 @@ const Header = () => {
         <img src='/assets/index9.jpeg' alt='picute1' />
       </div>
       <div className='text-header'>
-        <div className='satisfaction'>Votre satisfaction avant tout</div>
+        <div className='head-text'>Votre satisfaction avant tout</div>
         <div className='title-header'>
           <h1>TRAITEUR TRADITIONNEL</h1>
         </div>
@@ -27,9 +27,9 @@ const Header = () => {
         </div>
         <div className='text-content'>
           <p>Notre boutique traîteur en Seine et Marne est spécialisée en cuisine traditionnelle française.</p>
+          <br />
           <p>Nous vous proposons de nombreux plats régionaux et plats du terroir variés. Nous utilisons des produits frais et de saison.</p>
         </div>
-        <div className='vertical-line' />
       </div>
       <div className={display}>
         <Link to='/maison-morin'>
