92778dcddbacf728880fbc0937a0c8ce2ffe742c
Edouard-LARROCHE <edouard-larroche_student2021@wilder.school>
2022-02-24 17:04:29 +0100

    component third

--- front/src/components/home/DisplayScrollThird.jsx
+++ front/src/components/home/DisplayScrollThird.jsx
@@ -2,9 +2,30 @@ import React from 'react';
 
 const DisplayScrollThird = () => {
   return (
-    <div className='display-third'>
-      <h1>third display</h1>
-    </div>
+    <>
+      <div className='display-third'>
+        <div className='head-text-third'>TRAITEUR MAISON-MORIN</div>
+        <div className='head-title-third'>
+          <h1>PRESTATAIRE INCONTOURNABLE</h1>
+          <div className='V-position'>
+            <img className='V' src='/logo/V.png' alt='V' style={{ width: '2rem' }} />
+          </div>
+        </div>
+      </div>
+      <div className='presta-list'>
+        <ul>
+          <li>Buffet</li>
+          <li>Séminaire</li>
+          <li>Mariage</li>
+          <li>Naissance</li>
+          <li>Pot de départ</li>
+          <li>Baptême</li>
+          <li>Anniversaire</li>
+          <li>Cocktail</li>
+          <li>Repas assis</li>
+        </ul>
+      </div>
+    </>
   );
 };
 
