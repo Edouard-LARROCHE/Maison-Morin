76a28e636b2ec2fb5b9586b70d1ead111c86d5ea
Exemple Nom Uti <email@email.email>
2022-02-25 18:45:14 +0100

    infos

--- front/src/components/home/Infos.jsx
+++ front/src/components/home/Infos.jsx
@@ -1,31 +1,25 @@
 import React from "react";
 
 const Infos = () => {
-    return (
-        <div className="wrapper-info">
-            <div className="global-infos">
-            <div className="eco">
-                <img className="eco-img" src="/assets/ecologie.png" alt="" />
+    return (<>
+        <div className="panels-infos">
+            <div className="panel-left">
+                <div className="panel-left-overlay">
+                </div>
             </div>
-            <h2>Ecologie</h2>
-            <p>Nos packaging sont recyclables</p>
+            <div className="panel-center">
+                <div className="content-center-container"></div>
+                <div className="panel-center-img-bg">
+                    <div className="panel-center-overlay"></div>
+                    
+                </div>
             </div>
-            <div className="global-infos">
-            <div className="proxi">
-                <img className="proxi-img" src="/assets/proche.png" alt="" />
+            <div className="panel-right">
+                <div className="panel-right-overlay">
+                </div>
             </div>
-            <h2>Proximité</h2>
-            <p>Un circuit court, traditionnel</p>
-            </div>
-            <div className="global-infos">
-            <div className="pat">
-                <img className="pat-img" src="/assets/cupcake.png" alt="" />
-            </div>
-            <h2>Patisserie</h2>
-            <p>Goûtez notre délicieuse patisserie</p>
-            </div>                
         </div>
-    );
+    </>);
 };
 
 export default Infos;
