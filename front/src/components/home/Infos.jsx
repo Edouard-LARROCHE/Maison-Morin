79118f44d618d4e169fc9efc68e1387f12de7133
Exemple Nom Uti <email@email.email>
2022-02-28 16:40:55 +0100

    infos à la recherche du fix

--- front/src/components/home/Infos.jsx
+++ front/src/components/home/Infos.jsx
@@ -4,17 +4,39 @@ const Infos = () => {
     return (<>
         <div className="panels-infos">
             <div className="panel-left">
+                <div className="content-left-container">
+                    <div className="global-infos-left">
+                        <div className="first-half">
+                            <img className="eco-img" src="/assets/ecologie.png" alt="" />
+                            <p className="first-half-text">Ecologie</p>
+                            <p className="first-half-text-second">Nous faisons attention à l'envirronement</p>
+                            <div className="round">
+                                <img className="svg-button" src="/assets/add-svgrepo-com.svg" alt=""/>
+                            </div>
+                        </div>
+                        <div className="second-half">
+                            <h2>Lorem</h2>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus voluptates, eveniet recusandae facilis tempora, expedita hic voluptatibus nesciunt est tempore quia dolores mollitia doloremque. Neque libero itaque quos dolore non.
+                            In pariatur, tempore ab eius voluptates a neque quibusdam dolorem corporis molestiae, voluptatum soluta blanditiis numquam fuga doloribus commodi! Quo adipisci dignissimos impedit, tempore a iure fugiat nemo amet expedita?
+                            In, omnis nemo placeat natus adipisci quo nesciunt expedita ea veritatis harum, numquam dolore temporibus mollitia commodi recusandae reiciendis ad voluptatibus pariatur ex. Iste magni quibusdam dolorem porro eaque</p>
+                        </div>
+                    </div>
+                </div>
                 <div className="panel-left-overlay">
                 </div>
             </div>
             <div className="panel-center">
-                <div className="content-center-container"></div>
+                <div className="content-center-container">
+
+                </div>
                 <div className="panel-center-img-bg">
                     <div className="panel-center-overlay"></div>
-                    
                 </div>
             </div>
             <div className="panel-right">
+                <div className="content-right-container">
+                    
+                </div>
                 <div className="panel-right-overlay">
                 </div>
             </div>
@@ -23,3 +45,4 @@ const Infos = () => {
 };
 
 export default Infos;
+
