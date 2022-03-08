f4acb676b7be5b04dc21f3eeb2762b19d4cf2748
Edouard-LARROCHE <edouard-larroche_student2021@wilder.school>
2022-03-08 13:08:59 +0100

    gallery cake

--- front/src/components/gallery/traiteur/Poissons.jsx
+++ front/src/components/gallery/traiteur/Poissons.jsx
@@ -36,14 +36,20 @@ const Poissons = () => {
       ) : (
         <div className='fetch-card'>
           {data.map((index) => (
-            <div className='photos' key={index._id}>
-              <img className='img-gallerie' src={index.pictureUrl} alt='Photos-produits' />
-              <div className='icones'>
-                <div className='instagram'>
-                  <a className='fab fa-instagram' target='_blank' rel='noreferrer' href='https://www.instagram.com/maison_morin/?hl=fr'>
-                    {''}
-                  </a>
+            <div key={index._id}>
+              <div className='photos'>
+                <img className='img-gallerie' src={index.pictureUrl} alt='Photos-produits' />
+                <div className='icones'>
+                  <div className='instagram'>
+                    <a className='fab fa-instagram' target='_blank' rel='noreferrer' href='https://www.instagram.com/maison_morin/?hl=fr'>
+                      {''}
+                    </a>
+                  </div>
                 </div>
+              </div>
+              <div className='info-card'>
+                <p> {index.name} </p>
+                <p> {index.price} </p>
               </div>
             </div>
           ))}
