59967b823f7c876a8f420a68175025c94da4dc93
Edouard-LARROCHE <edouard-larroche_student2021@wilder.school>
2022-03-09 11:30:22 +0100

    drop without menu

---
+++ front/src/components/dropDown/DropWithoutMenu.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DropWithoutMenu = () => {
+  return (
+    <div>
+      <div className='drop-without-menu'></div>
+    </div>
+  );
+};
+
+export default DropWithoutMenu;
