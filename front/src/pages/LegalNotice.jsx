a271e1e4adbf9f6a2a1e297bd6c3a096740debfb
Edouard-LARROCHE <edouard-larroche_student2021@wilder.school>
2022-03-09 11:43:29 +0100

    import drop without

--- front/src/pages/LegalNotice.jsx
+++ front/src/pages/LegalNotice.jsx
@@ -6,12 +6,14 @@ import Copyright from '../components/legalNotice/Copyright';
 import UserTraces from '../components/legalNotice/UserTraces';
 import HeadBand from '../components/HeadBand';
 import TopArrow from '../components/TopArrow';
+import DropWithoutMenu from '../components/dropDown/DropWithoutMenu';
 
 const LegalNotice = () => {
   return (
     <div>
       <NavBar />
       <HeadBand />
+      <DropWithoutMenu />
       <div className='all-notice'>
         <Notice />
         <Copyright />
