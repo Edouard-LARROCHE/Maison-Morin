8367c386044cb92e3b90984ce2e641aaf50cba6f
Edouard-LARROCHE <edouard-larroche_student2021@wilder.school>
2022-03-11 12:15:50 +0100

    route:id

--- routes/pictureViandeRoute.js
+++ routes/pictureViandeRoute.js
@@ -9,6 +9,12 @@ pictureViande.get('/', (req, res) => {
     .catch((error) => res.status(400).json({ error }));
 });
 
+pictureViande.get('/:id', (req, res, next) => {
+  Viande.findOne({ _id: req.params.id })
+    .then((user) => res.status(200).json(user))
+    .catch((error) => res.status(404).json({ error }));
+});
+
 pictureViande.post('/', (req, res) => {
   const image = new Viande({
     ...req.body,
