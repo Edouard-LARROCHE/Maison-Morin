60f318265fe4b553c238ec6c1d7e106de780c156
Edouard-LARROCHE <edouard-larroche_student2021@wilder.school>
2022-03-14 22:12:50 +0100

    component cocktails

--- front/src/components/gallery/Cocktails.jsx
+++ front/src/components/gallery/Cocktails.jsx
@@ -1,79 +1,114 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import Loader from '../Loader';
+import Loader from '.././Loader';
+import { TextField } from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search';
+import Card from './Card';
 
 const Cocktails = () => {
-    const [loading, setLoading] = useState(true);
-    const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+  const [search, setSearch] = useState('');
+  const [itemsMore, setItemsMore] = useState(6);
 
-    useEffect(() => {
-        const fetchData = async () => {
-        setLoading(true);
-        try {
-            const { data: res } = await axios.get('http://localhost:5500');
-            setData(res);
-        } catch (err) {
-            console.log(err);
-        }
-        setLoading(false);
-        };
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const { data: res } = await axios.get('http://localhost:5500/posts');
+        setData(res);
+      } catch (err) {
+        console.log(err);
+      }
+      setLoading(false);
+    };
+    fetchData();
+    // eslint-disable-next-line
+  }, []);
 
-        fetchData();
-    }, []);
+  const handleSearch = (e) => {
+    let value = e.target.value;
+    setSearch(value);
+  };
 
-    return (
-        <>
-        <div className="Cocktails-headBand">
-        <div className="Infos-enterText">
-            <h1>COCKTAILS</h1>
-            <p className='Presentation-macarons'>Vous recevez des amis à manger chez vous? Vous organisez un anniversaire surprise? Un baptême? Ou simplement envie de vous faire plaisir?
-            Maison Maurin vous propose un large choix de canapés traiteur, petits fours de l'entrée au dessert pour vos apérititfs signés Victorien Morin et partager un moment de 
-            convivialité en toute tranquilité. 
+  const handleShowStep1 = () => {
+    setItemsMore(9, data.length);
+  };
+
+  const handleShowStep2 = () => {
+    setItemsMore(12, data.length);
+  };
+
+  const handleShowAll = () => {
+    setItemsMore(data.length);
+  };
+
+  return (
+    <>
+      <div className='Cocktails-headBand'>
+        <div className='Infos-enterText'>
+          <h1>COCKTAILS</h1>
+          <p className='Presentation-macarons'>
+            Vous recevez des amis à manger chez vous? Vous organisez un anniversaire surprise? Un baptême? Ou simplement envie de vous faire plaisir?
+            Maison Maurin vous propose un large choix de canapés traiteur, petits fours de l'entrée au dessert pour vos apérititfs signés Victorien
+            Morin et partager un moment de convivialité en toute tranquilité.
+          </p>
+        </div>
+      </div>
+      <div className='text-acceuil-fetch'>
+        <p>PRODUITS ARTISANAUX ELABORÉS DANS NOS CUISINES, AU QUOTIDIEN.</p>
+        <p style={{ fontSize: '10px', marginTop: '0.5rem' }}>NOTRE GAMME TRAITEUR</p>
+        <br />
+        <div className='line2' />
+        <br />
+        <div className='option-gallery'>
+          <div className='filter-display'>
+            <span>AFFICHER :</span>
+            <p onClick={handleShowStep1}>
+              <span>&#183;</span> 9
             </p>
-        </div>     
+            <p onClick={handleShowStep2}>
+              <span>&#183;</span>12
+            </p>
+            <p onClick={handleShowAll}>
+              <span>&#183;</span>24
+            </p>
+          </div>
+          <div className='search-bar'>
+            <TextField className='text-field' type='text' onChange={handleSearch} placeholder='Rechercher' style={{ color: '#f6fbf8' }} />
+            <SearchIcon style={{ cursor: 'pointer', color: '#012f6b' }} />
+            <p style={{ fontSize: '10px', marginTop: '0.5rem' }}>PAR NOM</p>
+          </div>
         </div>
-        <div className='text-acceuil-fetch'>
-            <p>PRODUITS ARTISANAUX ELABORÉS DANS NOS CUISINES, AU QUOTIDIEN.</p>
-            <p style={{ fontSize: '10px', marginTop: '0.5rem' }}>NOS PÂTISSERIES</p>
-            <br />
-            <div className='line2' />
+        <div className='line2' />
+      </div>
+      {loading ? (
+        <div>
+          <Loader />
         </div>
-        {loading ? (
-            <div>
-            <Loader />
-            </div>
-        ) : (
-            <div className='fetch-card'>
-            {data.map((index) => (
-                <div key={index._id}>
-                <div className='photos'>
-                    <img className='img-gallerie' src={index.pictureUrl} alt='Photos-produits' />
-                    <div className='icones'>
-                    <div className='instagram'>
-                        <a className='fab fa-instagram' target='_blank' rel='noreferrer' href='https://www.instagram.com/maison_morin/?hl=fr'>
-                        {''}
-                        </a>
-                    </div>
-                    </div>
-                </div>
-                <div className='info-card'>
-                    <p> {index.name} </p>
-                    <p> {index.price} </p>
-                </div>
-                </div>
+      ) : (
+        <div className='fetch-card'>
+          {data
+            .slice(0, itemsMore)
+            .filter((gallery) => {
+              return gallery.name.toLowerCase().includes(search.toLowerCase());
+            })
+            .map((gallery) => (
+              <Card key={gallery._id} gallery={gallery} />
             ))}
-            </div>
-        )}
-        <div className='text-order'>
-            <p>PASSER COMMANDE.</p>
-            <p style={{ fontSize: '10px', marginTop: '0.5rem' }}>
-            EN PRÉVISION D'UN FUTUR SERVICE DE COMMANDE, MERCI DE COMMANDER POUR L'INSTANT DIRECTEMENT PAR MAIL OU TÉLÉPHONE.
-            </p>
-            <br />
-            <div className='line2' />
         </div>
-        </>
-    );
+      )}
+
+      <div className='text-order'>
+        <p>PASSER COMMANDE.</p>
+        <p style={{ fontSize: '10px', marginTop: '0.5rem' }}>
+          EN PRÉVISION D'UN FUTUR SERVICE DE COMMANDE, MERCI DE COMMANDER POUR L'INSTANT DIRECTEMENT PAR MAIL OU TÉLÉPHONE.
+        </p>
+        <br />
+        <div className='line2' />
+      </div>
+    </>
+  );
 };
 
 export default Cocktails;
