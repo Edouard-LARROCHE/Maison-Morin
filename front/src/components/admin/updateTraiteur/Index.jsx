95eeb4e8fcb8867c2eeb3346f5d08aef1ce669d7
Edouard-LARROCHE <edouard-larroche_student2021@wilder.school>
2022-03-17 11:27:43 +0100

    promise.all

--- front/src/components/admin/updateTraiteur/Index.jsx
+++ front/src/components/admin/updateTraiteur/Index.jsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import IndexTraiteurViande from './updateViande/IndexTraiteurViande';
+import axios from 'axios';
 
 const Index = () => {
+  const [viande, setViande] = useState([]);
+  const [poisson, setPoisson] = useState([]);
+  const [charcuterie, setCharcuterie] = useState([]);
+
+  const allFetch = () => {
+    let urls = [
+      'http://localhost:5500/picture/traiteur/viande',
+      'http://localhost:5500/picture/traiteur/poisson',
+      'http://localhost:5500/picture/traiteur/charcuterie',
+    ];
+
+    Promise.all(urls.map((url) => axios.get(url))).then(([{ data: viande }, { data: poisson }, { data: charcuterie }]) => {
+      setViande(viande);
+      setPoisson(poisson);
+      setCharcuterie(charcuterie);
+      console.log(viande, poisson, charcuterie);
+    });
+  };
+
+  useEffect(() => {
+    allFetch();
+  }, []);
+
   return (
     <div>
       <IndexTraiteurViande />
--- front/src/components/admin/updateTraiteur/updateViande/IndexTraiteurViande.jsx
+++ front/src/components/admin/updateTraiteur/updateViande/IndexTraiteurViande.jsx
@@ -5,23 +5,37 @@ import UpdateForm from './UpdateForm';
 import PostForm from './PostForm';
 
 const IndexTraiteurViande = () => {
+  const [viande, setViande] = useState([]);
+  const [poisson, setPoisson] = useState([]);
+  const [charcuterie, setCharcuterie] = useState([]);
+
   const [data, setData] = useState([]);
   const [editing, setEditing] = useState(false);
 
   const initialForm = { _id: null, pictureUrl: '', name: '', price: '' };
   const [currentCard, setCurrentCard] = useState(initialForm);
 
+  const fetchData = async () => {
+    let urls = [
+      'http://localhost:5500/picture/traiteur/viande',
+      'http://localhost:5500/picture/traiteur/poisson',
+      'http://localhost:5500/picture/traiteur/charcuterie',
+    ];
+
+    Promise.all(urls.map((url) => axios.get(url)))
+      .then(([{ data: viande }, { data: poisson }, { data: charcuterie }]) => {
+        setViande(viande);
+        setPoisson(poisson);
+        setCharcuterie(charcuterie);
+        setData(viande, poisson, charcuterie);
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   useEffect(() => {
-    const fetchData = async () => {
-      await axios
-        .get('http://localhost:5500/picture/traiteur/viande')
-        .then((res) => {
-          setData(res.data);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    };
     fetchData();
     // eslint-disable-next-line
   }, []);
