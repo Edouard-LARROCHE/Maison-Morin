d593c94f34328f6831b90788055e5158087f26e5
Exemple Nom Uti <email@email.email>
2022-03-17 18:23:49 +0100

    text-fix

--- front/src/components/gallery/ProduitsDexception.jsx
+++ front/src/components/gallery/ProduitsDexception.jsx
@@ -59,7 +59,7 @@ const ProduitsDexception = () => {
       <div className="plus-text">
         <h3 className='t-eT'>Mais aussi</h3>
       <p>
-        Maison Morin vous propose son Saumon écossais fumé au bois de hêtre, un saumon ?? à chair ??? ?? et ferme avec des notes douces et dorées, un équilibre idéal, disponible
+        Maison Morin vous propose son Saumon écossais fumé au bois de hêtre, un saumon fondant à chair rosée et ferme avec des notes douces et dorées, un équilibre idéal, disponible
         en boutique tranchés par nos soins! <br></br><br></br> Craquez pour le caviar d'exception Impérial de Sologne by Maison Nordique
       </p>
     </div>
