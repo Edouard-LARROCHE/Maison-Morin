5604bd6ce95acda3bd79ac534b21096b2259c25c
Edouard-LARROCHE <edouard-larroche_student2021@wilder.school>
2022-03-17 16:55:58 +0100

    switch case

--- front/src/components/admin/updateTraiteur/Table.jsx
+++ front/src/components/admin/updateTraiteur/Table.jsx
@@ -4,7 +4,7 @@ import TableViande from './TableViande';
 import TablePoisson from './TablePoisson';
 
 const Table = (props) => {
-  const [show, setShow] = useState(false);
+  const [show, setShow] = useState('viande');
 
   const handleDelete = async (id) => {
     let popUp = window.confirm('Tu es sûr ?');
@@ -23,12 +23,43 @@ const Table = (props) => {
 
   return (
     <>
-      <div>
-        <TableViande viandes={props.viandes} editRow={props.editRow} deleteCard={props.deleteCard} handleDelete={handleDelete} />
-      </div>
-      <div>
-        <TablePoisson poissons={props.poissons} editRow={props.editRow} deleteCard={props.deleteCard} handleDelete={handleDelete} />
-      </div>
+      <ul>
+        <li onClick={() => setShow('viande')}>viande</li>
+        <li onClick={() => setShow('poisson')}>poisson</li>
+      </ul>
+      {(() => {
+        switch (show) {
+          case 'viande':
+            return (
+              <div>
+                <TableViande viandes={props.viandes} editRow={props.editRow} deleteCard={props.deleteCard} handleDelete={handleDelete} />
+              </div>
+            );
+
+          case 'poisson':
+            return (
+              <div>
+                <TablePoisson poissons={props.poissons} editRow={props.editRow} deleteCard={props.deleteCard} handleDelete={handleDelete} />
+              </div>
+            );
+
+          default:
+            return null;
+        }
+      })()}
+      {/* <ul>
+        <li onClick={() => setShow(true)}>viande</li>
+        <li onClick={() => setShow(false)}>poisson</li>
+      </ul>
+      {show ? (
+        <div>
+          <TableViande viandes={props.viandes} editRow={props.editRow} deleteCard={props.deleteCard} handleDelete={handleDelete} />
+        </div>
+      ) : (
+        <div>
+          <TablePoisson poissons={props.poissons} editRow={props.editRow} deleteCard={props.deleteCard} handleDelete={handleDelete} />
+        </div>
+      )} */}
     </>
   );
 };
