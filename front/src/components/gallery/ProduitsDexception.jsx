5c673ba486e405a3407b3e077a253f8f88e88cab
Edouard-LARROCHE <edouard-larroche_student2021@wilder.school>
2022-03-18 15:13:27 +0100

    create url

--- front/src/components/gallery/ProduitsDexception.jsx
+++ front/src/components/gallery/ProduitsDexception.jsx
@@ -15,7 +15,7 @@ const ProduitsDexception = () => {
     const fetchData = async () => {
       setLoading(true);
       try {
-        const { data: res } = await axios.get('http://localhost:5500/posts');
+        const { data: res } = await axios.get('http://localhost:5500/picture/produitsExcep');
         setData(res);
       } catch (err) {
         console.log(err);
@@ -56,13 +56,14 @@ const ProduitsDexception = () => {
           </p>
         </div>
       </div>
-      <div className="plus-text">
+      <div className='plus-text'>
         <h3 className='t-eT'>Mais aussi</h3>
-      <p>
-        Maison Morin vous propose son Saumon écossais fumé au bois de hêtre, un saumon fondant à chair rosée et ferme avec des notes douces et dorées, un équilibre idéal, disponible
-        en boutique tranchés par nos soins! <br></br><br></br> Craquez pour le caviar d'exception Impérial de Sologne by Maison Nordique
-      </p>
-    </div>
+        <p>
+          Maison Morin vous propose son Saumon écossais fumé au bois de hêtre, un saumon fondant à chair rosée et ferme avec des notes douces et
+          dorées, un équilibre idéal, disponible en boutique tranchés par nos soins! <br></br>
+          <br></br> Craquez pour le caviar d'exception Impérial de Sologne by Maison Nordique
+        </p>
+      </div>
       <div className='text-acceuil-fetch'>
         <p>PRODUITS ARTISANAUX ELABORÉS DANS NOS CUISINES, AU QUOTIDIEN.</p>
         <p style={{ fontSize: '10px', marginTop: '0.5rem' }}>NOTRE GAMME D'EXCEPTION</p>
