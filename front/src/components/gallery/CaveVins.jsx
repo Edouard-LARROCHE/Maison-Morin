95695005f10e1e8df36d995424f6cba63bf3de5c
Edouard-LARROCHE <edouard-larroche_student2021@wilder.school>
2022-03-18 16:49:51 +0100

    update cave vins

--- front/src/components/gallery/CaveVins.jsx
+++ front/src/components/gallery/CaveVins.jsx
@@ -1,80 +1,115 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Loader from '.././Loader';
+import { TextField } from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search';
+import Card from './Card';
 
 const CaveVins = () => {
-    const [loading, setLoading] = useState(true);
-    const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+  const [search, setSearch] = useState('');
+  const [itemsMore, setItemsMore] = useState(6);
 
-    useEffect(() => {
-        const fetchData = async () => {
-        setLoading(true);
-        try {
-            const { data: res } = await axios.get('http://localhost:5500/picture/cave/vins');
-            setData(res);
-        } catch (err) {
-            console.log(err);
-        }
-        setLoading(false);
-        };
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const { data: res } = await axios.get('http://localhost:5500/picture/cave/vins');
+        setData(res);
+      } catch (err) {
+        console.log(err);
+      }
+      setLoading(false);
+    };
 
-        fetchData();
-    }, []);
+    fetchData();
+    // eslint-disable-next-line
+  }, []);
 
-    return (
-        <>
-        <div className='Vins-headBand'>
-            <div className='Infos-enterText'>
-            <h1>VINS</h1>
-            <p className='Presentation-gallery'>
-                Maison Morin vous propose des vins de qualités, une diversité de vins pour satisfaire les goûts de chacun de nos clients, nos vins accompagneront vos repas, que ce soit
-                pour une grande occasion ou simplement pour vous faire plaisir à travers divers réferences tel que le Condrieu, Chateauneuf Du Pape, Pagodes de Cos, 
-                Chateau Clerc Milion Pauillac,  Sauternes-Barsac ou encore Minuty.
-                </p>
-            </div>
+  const handleSearch = (e) => {
+    let value = e.target.value;
+    setSearch(value);
+  };
+
+  const handleShowStep1 = () => {
+    setItemsMore(9, data.length);
+  };
+
+  const handleShowStep2 = () => {
+    setItemsMore(12, data.length);
+  };
+
+  const handleShowAll = () => {
+    setItemsMore(data.length);
+  };
+
+  return (
+    <>
+      <div className='Vins-headBand'>
+        <div className='Infos-enterText'>
+          <h1>VINS</h1>
+          <p className='Presentation-gallery'>
+            Maison Morin vous propose des vins de qualités, une diversité de vins pour satisfaire les goûts de chacun de nos clients, nos vins
+            accompagneront vos repas, que ce soit pour une grande occasion ou simplement pour vous faire plaisir à travers divers réferences tel que
+            le Condrieu, Chateauneuf Du Pape, Pagodes de Cos, Chateau Clerc Milion Pauillac, Sauternes-Barsac ou encore Minuty.
+          </p>
         </div>
-        <div className='text-acceuil-fetch'>
-            <p>UNE SELECTION ... </p>
-            <p style={{ fontSize: '10px', marginTop: '0.5rem' }}>NOTRE CAVE VIN ROUGE</p>
-            <br />
-            <div className='line2' />
-        </div>
-        {loading ? (
-            <div>
-            <Loader />
-            </div>
-        ) : (
-            <div className='fetch-card'>
-            {data.map((index) => (
-                <div key={index._id}>
-                <div className='photos'>
-                    <img className='img-gallerie' src={index.pictureUrl} alt='Photos-produits' />
-                    <div className='icones'>
-                    <div className='instagram'>
-                        <a className='fab fa-instagram' target='_blank' rel='noreferrer' href='https://www.instagram.com/maison_morin/?hl=fr'>
-                        {''}
-                        </a>
-                    </div>
-                    </div>
-                </div>
-                <div className='info-card'>
-                    <p> {index.name} </p>
-                    <p> {index.price} </p>
-                </div>
-                </div>
-            ))}
-            </div>
-        )}
-        <div className='text-order'>
-            <p>PASSER COMMANDE.</p>
-            <p style={{ fontSize: '10px', marginTop: '0.5rem' }}>
-            EN PRÉVISION D'UN FUTUR SERVICE DE COMMANDE, MERCI DE COMMANDER POUR L'INSTANT DIRECTEMENT PAR MAIL OU TÉLÉPHONE.
+      </div>
+      <div className='text-acceuil-fetch'>
+        <p>NOTRE SELECTION DE DIFFÉRENTS VINS ET CHAMPAGNES.</p>
+        <p style={{ fontSize: '10px', marginTop: '0.5rem' }}>NOTRE GAMME CAVE À VIN</p>
+        <br />
+        <div className='line2' />
+        <br />
+        <div className='option-gallery'>
+          <div className='filter-display'>
+            <span>AFFICHER :</span>
+            <p onClick={handleShowStep1}>
+              <span>&#183;</span> 9
             </p>
-            <br />
-            <div className='line2' />
+            <p onClick={handleShowStep2}>
+              <span>&#183;</span> 12
+            </p>
+            <p onClick={handleShowAll}>
+              <span>&#183;</span> 24
+            </p>
+          </div>
+          <div className='search-bar'>
+            <TextField className='text-field' type='text' onChange={handleSearch} placeholder='Rechercher' style={{ color: '#f6fbf8' }} />
+            <SearchIcon style={{ cursor: 'pointer', color: '#012f6b' }} />
+            <p style={{ fontSize: '10px', marginTop: '0.5rem' }}>PAR NOM</p>
+          </div>
         </div>
-        </>
-    );
+        <div className='line2' />
+      </div>
+      {loading ? (
+        <div>
+          <Loader />
+        </div>
+      ) : (
+        <div className='fetch-card'>
+          {data
+            .slice(0, itemsMore)
+            .filter((gallery) => {
+              return gallery.name.toLowerCase().includes(search.toLowerCase());
+            })
+            .map((gallery) => (
+              <Card key={gallery._id} gallery={gallery} />
+            ))}
+        </div>
+      )}
+
+      <div className='text-order'>
+        <p>PASSER COMMANDE.</p>
+        <p style={{ fontSize: '10px', marginTop: '0.5rem' }}>
+          EN PRÉVISION D'UN FUTUR SERVICE DE COMMANDE, MERCI DE COMMANDER POUR L'INSTANT DIRECTEMENT PAR MAIL OU TÉLÉPHONE.
+        </p>
+        <br />
+        <div className='line2' />
+      </div>
+    </>
+  );
 };
 
 export default CaveVins;
