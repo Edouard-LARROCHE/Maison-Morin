1ff50c073dda5cf33b1c1ab6b0826bd931dbc781
Exemple Nom Uti <email@email.email>
2022-03-21 13:20:31 +0100

    band accueil

--- front/src/components/home/Band.jsx
+++ front/src/components/home/Band.jsx
@@ -2,8 +2,15 @@ import React from 'react';
 
 const Band = () => {
   return (
-    <div>
-      <h1>???</h1>
+    <div className='Band-enter'>
+      <div className="band-txt">
+        <h1>Maison Morin</h1>
+        <p className='txt-in'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, ratione quibusdam! Assumenda consectetur autem molestiae quibusdam quasi tempora tenetur eveniet, 
+          rem, amet est quam repellendus praesentium voluptatum repellat ratione libero!</p>
+          <div className="btn-band">
+            <p>Visiter</p>
+          </div>
+      </div>
     </div>
   );
 };
