6fc7fb6cfc75e76ac82138fc63b406855f489a50
Edouard-LARROCHE <edouard-larroche_student2021@wilder.school>
2022-03-21 16:28:43 +0100

    link picture menu

--- front/src/components/menu/CardMenu.jsx
+++ front/src/components/menu/CardMenu.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const CardMenu = (props) => {
   return (
-    <div className='card-menu'>
-      <img src={props.img} alt='MAISON-MORIN' />
-      <h2 className='desc'> {props.descHover} </h2>
-      <h1 className='desc-hover'> {props.desc} </h1>
-    </div>
+    <>
+      <Link to={`/${props.link}`}>
+        <div className='card-menu'>
+          <img src={props.img} alt='MAISON-MORIN' />
+          <h2 className='desc'> {props.descHover} </h2>
+          <h1 className='desc-hover'> {props.desc} </h1>
+        </div>
+      </Link>
+    </>
   );
 };
 
--- front/src/components/menu/DataMenu.jsx
+++ front/src/components/menu/DataMenu.jsx
@@ -5,36 +5,42 @@ const DataMenu = () => {
   const dataMenu = [
     {
       id: 1,
+      link: 'traiteur-charcuterie',
       img: '/assets/traiteur.png',
       desc: 'TRAITEUR',
       descHover: 'TRAITEUR',
     },
     {
       id: 2,
+      link: 'patisseries',
       img: '/assets/patisserie10.jpeg',
       desc: 'PÂTISSERIES',
       descHover: 'PÂTISSERIES',
     },
     {
       id: 3,
+      link: 'patisseries-macarons',
       img: '/assets/selection-macaron2.jpeg',
       desc: 'MACARONS',
       descHover: 'MACARONS',
     },
     {
       id: 4,
+      link: 'cave-a-vins',
       img: '/assets/Fond-vins-45.png',
       desc: 'CAVE À VINS',
       descHover: 'CAVE À VINS',
     },
     {
       id: 5,
+      link: 'cocktails',
       img: '/assets/cocktails-dinatoir.jpeg',
       desc: 'COCKTAILS DINATOIRE',
       descHover: 'COCKTAILS DINATOIRE',
     },
     {
       id: 6,
+      link: "produits-d'exception",
       img: '/assets/prod-except.jpeg',
       desc: "PRODUITS D'EXCEPTION",
       descHover: "PRODUITS D'EXCEPTION",
@@ -45,7 +51,7 @@ const DataMenu = () => {
     <div className='global-card-menu'>
       {dataMenu.map((index) => (
         <div key={index.id}>
-          <CardMenu img={index.img} desc={index.desc} descHover={index.descHover} />
+          <CardMenu img={index.img} desc={index.desc} descHover={index.descHover} link={index.link} />
         </div>
       ))}
     </div>
