caee85e51d9e970f6a05edfa8fe1ded536d323a1
Edouard-LARROCHE <edouard-larroche_student2021@wilder.school>
2022-03-22 09:11:36 +0100

    style component band

--- front/src/components/home/Band.jsx
+++ front/src/components/home/Band.jsx
@@ -3,13 +3,16 @@ import React from 'react';
 const Band = () => {
   return (
     <div className='Band-enter'>
-      <div className="band-txt">
-        <h1>Maison Morin</h1>
-        <p className='txt-in'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, ratione quibusdam! Assumenda consectetur autem molestiae quibusdam quasi tempora tenetur eveniet, 
-          rem, amet est quam repellendus praesentium voluptatum repellat ratione libero!</p>
-          <div className="btn-band">
-            <p>Visiter</p>
-          </div>
+      <div className='band-txt'>
+        <h1>MAISON MORIN</h1>
+        <p className='txt-in'>Notre boutique traîteur en Seine et Marne est spécialisée en cuisine traditionnelle française.</p>
+        <br />
+        <p className='txt-in'>
+          Nous vous proposons de nombreux plats régionaux et plats du terroir variés. Nous utilisons des produits frais et de saison.
+        </p>
+        <div className='btn-band'>
+          <p>Découvrir</p>
+        </div>
       </div>
     </div>
   );
