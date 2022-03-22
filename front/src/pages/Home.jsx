9449298c070c79ff7c847c723d54c4dee3751bd6
Edouard-LARROCHE <edouard-larroche_student2021@wilder.school>
2022-03-22 09:18:23 +0100

    delete file home

--- front/src/pages/Home.jsx
+++ front/src/pages/Home.jsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import NavBar from '../components/home/NavBar';
 import MenuDrop from '../components/dropDown/MenuDrop';
-// import Header from '../components/home/Header';
 import Footer from '../components/home/Footer';
 import Infos from '../components/home/Infos';
 import NewsLetter from '../components/newsletter/NewsLetter';
 import HeadBand from '../components/HeadBand';
 import Band from '../components/home/Band';
-// import DisplayScroll from '../components/home/DisplayScrollFirst';
-// import DisplayScrollSecond from '../components/home/DisplayScrollSecond';
-// import DisplayScrollThird from '../components/home/DisplayScrollThird';
 import Avis from '../components/Avis';
 import TopArrow from '../components/TopArrow';
 import Selection from '../components/Selection';
@@ -27,10 +23,6 @@ const Home = () => {
         <Selection />
         <Menu />
         <BottomCard />
-        {/* <Header />
-        <DisplayScroll />
-        <DisplayScrollSecond />
-        <DisplayScrollThird /> */}
         <Infos />
         <Avis />
         <NewsLetter />
