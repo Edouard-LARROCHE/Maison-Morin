ee443f461c8294dbc1ee78805c535d494777d0b6
Edouard-LARROCHE <edouard-larroche_student2021@wilder.school>
2022-03-28 10:51:16 +0200

    indentation new-letter

--- front/src/components/newsletter/NewsLetter.jsx
+++ front/src/components/newsletter/NewsLetter.jsx
@@ -12,10 +12,12 @@ const NewsLetter = () => {
         <div className='text-info-nl'>
           <h3>Soyez les premiers à être informés de nos actualités !</h3>
         </div>
-        <MailchimpSubscribe url={'https://gmail.us14.list-manage.com/subscribe/post?u=3d643a3976eec9ec481f6a43f&amp;id=f9244b0c4c'} render={({ subscribe, status, message }) => (
-              <CustomForm status={status} message={message} onValidated={formData => subscribe(formData)} />
-                )}
-            />
+        <MailchimpSubscribe
+          url={'https://gmail.us14.list-manage.com/subscribe/post?u=3d643a3976eec9ec481f6a43f&amp;id=f9244b0c4c'}
+          render={({ subscribe, status, message }) => (
+            <CustomForm status={status} message={message} onValidated={(formData) => subscribe(formData)} />
+          )}
+        />
       </div>
     </>
   );
