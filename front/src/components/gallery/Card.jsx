e39a274508d4d87962d56fd43123a1e71bbea7f7
Edouard-LARROCHE <edouard-larroche_student2021@wilder.school>
2022-04-01 15:48:58 +0200

    ...

--- front/src/components/gallery/Card.jsx
+++ front/src/components/gallery/Card.jsx
@@ -4,19 +4,13 @@ import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 const Card = ({ gallery }) => {
   const addStorage = () => {
     if (localStorage.getItem('token')) {
-      const effect = document.querySelector('.add-shopping-cart');
-      effect.style.color = ' #ddb355';
       let storeData = window.localStorage.card ? window.localStorage.card.split(',') : [];
-
       if (!storeData.includes(gallery._id.toString())) {
         storeData.push(gallery._id);
         window.localStorage.card = storeData;
-      } else if (storeData) {
-        const popErr = document.querySelector('.pop-err');
-        alert('déjà ajouter');
-      } else {
-        alert('connexion requise');
       }
+    } else {
+      alert('connexion requise');
     }
   };
 
@@ -37,7 +31,9 @@ const Card = ({ gallery }) => {
           <p style={{ marginRight: '0.5rem' }}>AJOUTER AU PANIER</p>
           <ShoppingCartIcon style={{ transform: 'translateY(15px)' }} />
         </div>
-        <div className='pop-err' />
+      </div>
+      <div className='pop-err'>
+        <p>AJOUTER</p>
       </div>
     </div>
   );
