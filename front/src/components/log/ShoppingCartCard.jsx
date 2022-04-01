d4993429949c097540962cf0e1118882f9bc991d
Edouard-LARROCHE <edouard-larroche_student2021@wilder.school>
2022-04-01 12:03:08 +0200

    delete card

--- front/src/components/log/ShoppingCartCard.jsx
+++ front/src/components/log/ShoppingCartCard.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 
 const ShoppingCartCard = ({ gallery }) => {
+  const deleteStorage = () => {
+    let storedData = window.localStorage.card.split(',');
+    let newData = storedData.filter((id) => id !== gallery._id);
+    window.localStorage.card = newData;
+  };
+
   return (
     <div className='photos'>
       <img className='img-gallerie' src={gallery.pictureUrl} alt='MAISON-MORIN' />
@@ -8,6 +14,13 @@ const ShoppingCartCard = ({ gallery }) => {
       <div className='info-card'>
         <p> {gallery.name} </p>
         <p> {gallery.price} </p>
+        <button
+          onClick={() => {
+            deleteStorage();
+            window.location.reload();
+          }}>
+          <p>SUPPRIMER AU PANIER</p>
+        </button>
       </div>
     </div>
   );
