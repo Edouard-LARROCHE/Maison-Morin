b534c3bd37ea5e5d9ece8ce0a560f89c652db9aa
Edouard-LARROCHE <edouard-larroche_student2021@wilder.school>
2022-04-02 14:51:54 +0200

    promise all url shopping cart

--- front/src/components/log/ShoppingCartData.jsx
+++ front/src/components/log/ShoppingCartData.jsx
@@ -7,12 +7,32 @@ const ShoppingCartData = () => {
 
   useEffect(() => {
     let cardDataId = window.localStorage.card ? window.localStorage.card.split(',') : [];
-    let urls = ['http://localhost:5500/picture/cave/vins/', 'http://localhost:5500/picture/traiteur/viande/'];
+    let urls = [
+      'http://localhost:5500/picture/cave/vins/',
+      'http://localhost:5500/picture/traiteur/viande/',
+      'http://localhost:5500/picture/traiteur/poisson',
+      'http://localhost:5500/picture/traiteur/charcuterie',
+      'http://localhost:5500/picture/patisserie/macaron',
+      'http://localhost:5500/picture/patisserie/patisseries',
+      'http://localhost:5500/picture/cocktails',
+      'http://localhost:5500/picture/produitsExcep',
+    ];
 
     for (let i = 0; i < cardDataId.length; i++) {
-      Promise.all(urls.map((url) => axios.get(url + cardDataId[i]))).then(([{ data: vins }, { data: viandes }]) => {
-        setListData((listData) => [...listData, vins || viandes]);
-      });
+      Promise.all(urls.map((url) => axios.get(url + cardDataId[i]))).then(
+        ([
+          { data: vins },
+          { data: viandes },
+          { data: poisson },
+          { data: charcuterie },
+          { data: macaron },
+          { data: patisserie },
+          { data: cocktails },
+          { data: produitsexcep },
+        ]) => {
+          setListData((listData) => [...listData, vins || viandes || poisson || charcuterie || macaron || patisserie || cocktails || produitsexcep]);
+        },
+      );
     }
   }, []);
 
