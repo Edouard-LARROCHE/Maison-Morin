a96d0876a3ef555ab168fb95f597eb7cb77fe14e
Edouard-LARROCHE <edouard-larroche_student2021@wilder.school>
2022-04-02 14:56:22 +0200

    route /:id poisson

--- routes/picturePoissonRoute.js
+++ routes/picturePoissonRoute.js
@@ -9,6 +9,12 @@ picturePoisson.get('/', (req, res) => {
     .catch((error) => res.status(400).json({ error }));
 });
 
+picturePoisson.get('/:id', (req, res, next) => {
+  Poisson.findOne({ _id: req.params.id })
+    .then((user) => res.status(200).json(user))
+    .catch((error) => res.status(404).json({ error }));
+});
+
 picturePoisson.post('/', (req, res) => {
   const image = new Poisson({
     ...req.body,
