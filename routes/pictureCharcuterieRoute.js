9b42dc9eea86c4a46b732bb495c03a611141b95b
Edouard-LARROCHE <edouard-larroche_student2021@wilder.school>
2022-04-02 14:53:27 +0200

    route /:id charcuterie

--- routes/pictureCharcuterieRoute.js
+++ routes/pictureCharcuterieRoute.js
@@ -9,6 +9,12 @@ pictureCharcuterie.get('/', (req, res) => {
     .catch((error) => res.status(400).json({ error }));
 });
 
+pictureCharcuterie.get('/:id', (req, res, next) => {
+  Charcuterie.findOne({ _id: req.params.id })
+    .then((user) => res.status(200).json(user))
+    .catch((error) => res.status(404).json({ error }));
+});
+
 pictureCharcuterie.post('/', (req, res) => {
   const image = new Charcuterie({
     ...req.body,
