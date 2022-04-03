bbe01c165114a9bac1ac53a7f84fe04473acbbd4
Exemple Nom Uti <email@email.email>
2022-04-03 13:56:21 +0200

    fixs..

--- front/src/components/home/Band.jsx
+++ front/src/components/home/Band.jsx
@@ -8,7 +8,7 @@ const Band = () => {
         <h1>MAISON MORIN</h1>
         <p className='txt-in'>Nous sélectionnons avec soin nos fournisseurs et privilégions dans la mesure du possible des productions locales.</p>
         <p className='txt-in'>
-          Nous favorisons quotidiennement des produits frais pour valoriser une offre de saison. Fraicheur et naturalité sont conjuguées pour exhaler
+          Nous favorisons quotidiennement des produits frais pour valoriser une offre de saison. Fraîcheur et naturalité sont conjuguées pour exhaler
           davantage la qualité gustative de nos recettes.
         </p>
         <div className='btn-band'>
