3349b9d4d9ac8ae81500a876f3db01bcedc72d53
Edouard-LARROCHE <edouard-larroche_student2021@wilder.school>
2022-04-04 12:46:10 +0200

    add noopner

--- front/src/components/Avis.jsx
+++ front/src/components/Avis.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import {Swiper, SwiperSlide} from 'swiper/react';
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
 import { Autoplay, Pagination, Navigation } from 'swiper';
 
 // Style Swiper
@@ -8,94 +8,109 @@ import 'swiper/scss/pagination';
 import 'swiper/scss/navigation';
 
 export default function Avis() {
-    return (
-        <div className="container-avis">              
-            <div className='avis-btn'>
-                <a href="https://g.page/r/Ca2x0BESGrArEAo/review" target='_blank'>Donner un avis Google</a>
+  return (
+    <div className='container-avis'>
+      <div className='avis-btn'>
+        <a href='https://g.page/r/Ca2x0BESGrArEAo/review' target='_blank' rel='noreferrer noopener'>
+          Donner un avis Google
+        </a>
+      </div>
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={30}
+        loop={true}
+        autoplay={{
+          delay: 7000,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Autoplay, Pagination, Navigation]}
+        className='mySwiper'>
+        <SwiperSlide>
+          <div className='comment-box-sp'>
+            <h3> “ Toujours excellent, un vrai plaisir pour les papilles. Un accueil extra et une équipe fort sympathique. ”</h3>
+            <div className='arrow-sp'></div>
+            <div className='comment-author-sp'>
+              <h1>Stéphanie Pestel</h1>
             </div>
-        <Swiper
-            slidesPerView={1}
-            spaceBetween={30}
-            loop={true}
-            autoplay={{
-                delay: 7000,
-                disableOnInteraction: false,
-            }}
-            pagination={{
-            clickable: true,
-            }}
-            navigation={true}
-            modules={[Autoplay, Pagination, Navigation]}
-            className="mySwiper"
-            >
-            <SwiperSlide>
-            <div className="comment-box-sp">
-                <h3> “ Toujours excellent, un vrai plaisir pour les papilles. Un accueil extra et une équipe fort sympathique. ”</h3>
-                <div className="arrow-sp"></div>
-                <div className="comment-author-sp">
-                    <h1>Stéphanie Pestel</h1>
-                </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className='comment-box-stp'>
+            <h3> “ Super accueil et de très bon conseils. Une cuisine délicieuse et raffinée. Je recommande a 100% ”</h3>
+            <div className='arrow-stp'></div>
+            <div className='comment-author-stp'>
+              <h1>Cauline Rolmilaguet</h1>
             </div>
-            </SwiperSlide>
-            <SwiperSlide>
-            <div className="comment-box-stp">
-                <h3> “ Super accueil et de très bon conseils. Une cuisine délicieuse et raffinée. Je recommande a 100% ”</h3>
-                <div className="arrow-stp"></div>
-                <div className="comment-author-stp">
-                    <h1>Cauline Rolmilaguet</h1>
-                </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className='comment-box-sl'>
+            <h3> “ Très bon traiteur et en plus très accueillant!Je recommande!! ”</h3>
+            <div className='arrow-sl'></div>
+            <div className='comment-author-sl'>
+              <h1>Stéphane Leblanc</h1>
             </div>
-            </SwiperSlide>
-            <SwiperSlide>
-            <div className="comment-box-sl">
-                <h3> “ Très bon traiteur et en plus très accueillant!Je recommande!! ”</h3>
-                <div className="arrow-sl"></div>
-                <div className="comment-author-sl">
-                    <h1>Stéphane Leblanc</h1>
-                </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className='comment-box-actu'>
+            <h3>
+              {' '}
+              “ C’est un véritable passage de témoins père-fils qui a eu lieu autour de la Maison Morin à Barbizon (Seine-et-Marne), très prisée des
+              habitants et des touristes... ”{' '}
+              <a
+                href='https://actu.fr/ile-de-france/barbizon_77022/seine-et-marne-a-barbizon-la-maison-morin-change-dans-la-continuite_46487631.html'
+                target='_blank'
+                rel='noreferrer noopener'>
+                lire l'article
+              </a>
+            </h3>
+            <div className='arrow-actu'></div>
+            <div className='comment-author-actu'>
+              <h1>Actu.fr</h1>
             </div>
-            </SwiperSlide>
-            <SwiperSlide>
-            <div className="comment-box-actu">
-                <h3> “ C’est un véritable passage de témoins père-fils qui a eu lieu autour de la Maison Morin à Barbizon (Seine-et-Marne), 
-                    très prisée des habitants et des touristes... ” <a href="https://actu.fr/ile-de-france/barbizon_77022/seine-et-marne-a-barbizon-la-maison-morin-change-dans-la-continuite_46487631.html" target='_blank'>lire l'article</a></h3>
-                <div className="arrow-actu"></div>
-                <div className="comment-author-actu">
-                    <h1>Actu.fr</h1>
-                </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className='comment-box-kn'>
+            <h3>
+              {' '}
+              “ Très bon traiteur sur Barbizon. Les produits sont frais et de qualité. Tout est fait maison, c'est très agréable, nous nous sommes
+              régalés ! Accueil chaleureux. Bons conseils. Je recommande sans hésiter ! ”
+            </h3>
+            <div className='arrow-kn'></div>
+            <div className='comment-author-kn'>
+              <h1>Kelly Naar</h1>
             </div>
-            </SwiperSlide>
-            <SwiperSlide>
-            <div className="comment-box-kn">
-                <h3> “ Très bon traiteur sur Barbizon. Les produits sont frais et de qualité. Tout est fait maison, 
-                    c'est très agréable, nous nous sommes régalés ! Accueil chaleureux. Bons conseils. Je recommande sans hésiter ! ”</h3>
-                <div className="arrow-kn"></div>
-                <div className="comment-author-kn">
-                    <h1>Kelly Naar</h1>
-                </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className='comment-box-cm'>
+            <h3>
+              {' '}
+              “ Produits frais, maisons et de qualité ! Très bon accueil. Nous avons apprécié les conseils donnés notamment sur la cuisson de nos
+              plats. La boutique est magnifiquement décorée. Nous reviendrons sans hésiter. ”
+            </h3>
+            <div className='arrow-cm'></div>
+            <div className='comment-author-cm'>
+              <h1>Chachou M</h1>
             </div>
-            </SwiperSlide>
-            <SwiperSlide>
-            <div className="comment-box-cm">
-                <h3> “ Produits frais, maisons et de qualité ! Très bon accueil. Nous avons apprécié les conseils donnés notamment sur la cuisson de nos plats.
-                    La boutique est magnifiquement décorée. Nous reviendrons sans hésiter. ”</h3>
-                <div className="arrow-cm"></div>
-                <div className="comment-author-cm">
-                    <h1>Chachou M</h1>
-                </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className='comment-box-vd'>
+            <h3> “ Très bon charcutier traiteur, les terrines de lapin sont excellentes. ”</h3>
+            <div className='arrow-vd'></div>
+            <div className='comment-author-vd'>
+              <h1>Valerie Damas</h1>
             </div>
-            </SwiperSlide>
-            <SwiperSlide>
-            <div className="comment-box-vd">
-                <h3> “ Très bon charcutier traiteur, les terrines de lapin sont excellentes. ”</h3>
-                <div className="arrow-vd"></div>
-                <div className="comment-author-vd">
-                    <h1>Valerie Damas</h1>
-                </div>
-            </div>
-            </SwiperSlide>
-        </Swiper>
-        </div>
-    );
+          </div>
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
 }
-    
