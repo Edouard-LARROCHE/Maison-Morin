7c867e100a520f1d6f70a689cc4df214dc7c9ac2
Edouard-LARROCHE <edouard-larroche_student2021@wilder.school>
2022-04-04 14:28:45 +0200

    Maison-Morin

--- front/src/components/menu/Menu.jsx
+++ front/src/components/menu/Menu.jsx
@@ -7,7 +7,7 @@ const Menu = () => {
       <h1>NOTRE CARTE</h1>
       <div className='text-selection-menu'>
         <p>NOTRE CARTE ACTUELLE</p>
-        <p style={{ fontSize: '10px', marginTop: '0.5rem' }}>POUR EN SAVOIR PLUS SUR NOTRE CARTE, RENDEZ-VOUS SUR LA GALLERIE</p>
+        <p style={{ fontSize: '10px', marginTop: '0.5rem' }}>POUR EN SAVOIR PLUS SUR NOTRE CARTE, RENDEZ-VOUS SUR LA GALERIE</p>
         <br />
         <div className='line2' />
       </div>
