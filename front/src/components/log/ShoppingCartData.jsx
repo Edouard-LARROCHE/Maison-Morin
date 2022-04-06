6de47d939f57e245dca98c76feba77aaf4ceefeb
Edouard-LARROCHE <edouard-larroche_student2021@wilder.school>
2022-04-06 13:29:42 +0200

    Maison-Morin

--- front/src/components/log/ShoppingCartData.jsx
+++ front/src/components/log/ShoppingCartData.jsx
@@ -8,14 +8,14 @@ const ShoppingCartData = () => {
   useEffect(() => {
     let cardDataId = window.localStorage.card ? window.localStorage.card.split(',') : [];
     let urls = [
-      'http://localhost:5500/picture/cave/vins/',
-      'http://localhost:5500/picture/traiteur/viande/',
-      'http://localhost:5500/picture/traiteur/poisson',
-      'http://localhost:5500/picture/traiteur/charcuterie',
-      'http://localhost:5500/picture/patisserie/macaron',
-      'http://localhost:5500/picture/patisserie/patisseries',
-      'http://localhost:5500/picture/cocktails',
-      'http://localhost:5500/picture/produitsExcep',
+      '/picture/cave/vins/',
+      '/picture/traiteur/viande/',
+      '/picture/traiteur/poisson',
+      '/picture/traiteur/charcuterie',
+      '/picture/patisserie/macaron',
+      '/picture/patisserie/patisseries',
+      '/picture/cocktails',
+      '/picture/produitsExcep',
     ];
 
     for (let i = 0; i < cardDataId.length; i++) {
