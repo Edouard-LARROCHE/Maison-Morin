fb66b6e35b3783fd7e878651e7723c9df259af1a
Edouard-LARROCHE <edouard-larroche_student2021@wilder.school>
2022-04-21 10:47:42 +0200

    Maison-Morin

--- front/src/components/userInfo/ShoppingCartCard.jsx
+++ front/src/components/userInfo/ShoppingCartCard.jsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Loader from '../Loader';
 import DeleteIcon from '@material-ui/icons/Delete';
 
 const ShoppingCartCard = ({ gallery }) => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(false);
+  }, []);
+
   const deleteStorage = () => {
     let storedData = window.localStorage.card.split(',');
     let newData = storedData.filter((id) => id !== gallery._id);
@@ -9,24 +16,31 @@ const ShoppingCartCard = ({ gallery }) => {
   };
 
   return (
-    <div className='photos'>
-      <img className='img-gallerie' src={gallery.pictureUrl} alt='MAISON-MORIN' />
-      <div className='icones'></div>
-      <div className='info-card'>
-        <p> {gallery.name} </p>
-        <p> {gallery.price} </p>
-
-        <div
-          className='add-shopping-cart'
-          onClick={() => {
-            deleteStorage();
-            window.location.reload();
-          }}>
-          <p style={{ marginRight: '0.5rem' }}>SUPPRIMER AU PANIER</p>
-          <DeleteIcon style={{ transform: 'translateY(15px)' }} />
+    <>
+      {loading ? (
+        <>
+          <Loader />
+        </>
+      ) : (
+        <div className='photos'>
+          <img className='img-gallerie' src={gallery.pictureUrl} alt='MAISON-MORIN' />
+          <div className='icones'></div>
+          <div className='info-card'>
+            <p> {gallery.name} </p>
+            <p> {gallery.price} </p>
+            <div
+              className='add-shopping-cart'
+              onClick={() => {
+                deleteStorage();
+                window.location.reload();
+              }}>
+              <p style={{ marginRight: '0.5rem' }}>SUPPRIMER AU PANIER</p>
+              <DeleteIcon style={{ transform: 'translateY(15px)' }} />
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
