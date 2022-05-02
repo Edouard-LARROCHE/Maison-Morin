c3132194854dcf0fce9f8ceece0b90a8f0eb606a
Edouard-LARROCHE <edouard-larroche_student2021@wilder.school>
2022-05-02 14:14:40 +0200

    Maison-Morin

--- front/src/components/userInfo/Account.jsx
+++ front/src/components/userInfo/Account.jsx
@@ -1,12 +1,17 @@
-import React, { useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import axios from 'axios';
 import { useSelector } from 'react-redux';
 import { UidContext } from '../../AppContext';
 import { dateParser } from './DateConnect';
 
 const Account = ({ handleLogout }) => {
+  const [valid, setValid] = useState('valid-before');
   const userData = useSelector((state) => state.userReducer);
   const uid = useContext(UidContext);
+
+  const close = () => {
+    setValid('valid-before');
+  };
 
   const deleteAccount = async () => {
     await axios
@@ -27,10 +32,15 @@ const Account = ({ handleLogout }) => {
         <h3>Nom : {userData.lastName}</h3>
         <h3>Adresse e-mail : {userData.email}</h3>
         <h3>Compte créé le : {dateParser(userData.createdAt)}</h3>
-        <div>
-          <button style={{ margin: '1rem' }} className='log-button ' onClick={deleteAccount}>
+        <div className='delete-account'>
+          <button style={{ margin: '2rem' }} className='log-button ' onClick={() => setValid()}>
             <p>SUPRIMER MON COMPTE</p>
           </button>
+          <div className={valid}>
+            <p>confirmation de supréssion</p>
+            <p onClick={close}>non</p>
+            <p onClick={deleteAccount}>oui</p>
+          </div>
         </div>
       </div>
     </>
