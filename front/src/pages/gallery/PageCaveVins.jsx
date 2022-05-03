9e8bf5b2025ca57e082242038dba67770a46335f
Edouard-LARROCHE <edouard-larroche_student2021@wilder.school>
2022-05-03 16:35:35 +0200

    Maison-Morin

--- front/src/pages/gallery/PageCaveVins.jsx
+++ front/src/pages/gallery/PageCaveVins.jsx
@@ -1,21 +1,38 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Footer from '../../components/home/Footer';
 import NavBar from '../../components/home/NavBar';
 import HeadBand from '../../components/HeadBand';
 import MenuDrop from '../../components/dropDown/MenuDrop';
 import CaveVins from '../../components/gallery/CaveVins';
 import TopArrow from '../../components/TopArrow';
+import UserBar from '../../components/UserBar';
+import { UidContext } from '../../AppContext';
 
 const PageCaveVins = () => {
+  const uid = useContext(UidContext);
   return (
-    <div>
-      <NavBar />
-      <HeadBand />
-      <MenuDrop />
-      <CaveVins />
-      <Footer />
-      <TopArrow />
-    </div>
+    <>
+      {uid ? (
+        <div>
+          <UserBar />
+          <NavBar />
+          <HeadBand />
+          <MenuDrop />
+          <CaveVins />
+          <Footer />
+          <TopArrow />
+        </div>
+      ) : (
+        <div>
+          <NavBar />
+          <HeadBand />
+          <MenuDrop />
+          <CaveVins />
+          <Footer />
+          <TopArrow />
+        </div>
+      )}
+    </>
   );
 };
 
