1a3538e219513eef9c549fe26007088377ec33d6
Edouard-LARROCHE <edouard-larroche_student2021@wilder.school>
2022-05-03 15:32:35 +0200

    Maison-Morin

--- front/src/components/UserBar.jsx
+++ front/src/components/UserBar.jsx
@@ -1,8 +1,13 @@
 import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 
 const UserBar = () => {
+  const userFirstName = useSelector((state) => state.userReducer.firstName[0].toUpperCase());
+  const userLastName = useSelector((state) => state.userReducer.lastName[0].toUpperCase());
   const [bar, setBar] = useState('user-bar-before');
+
+  const user = userFirstName + '.' + userLastName;
 
   const displayBar = () => {
     if (window.scrollY >= 500) {
@@ -16,12 +21,17 @@ const UserBar = () => {
 
   return (
     <div className={bar}>
-      <p>
-        MON PANIER
-        <span>
-          <ShoppingCartIcon style={{ color: '#fff', fontSize: '15px', marginLeft: '0.2rem' }} />
-        </span>
-      </p>
+      <div className='shopCart-bar'>
+        <p>
+          MON PANIER
+          <span>
+            <ShoppingCartIcon style={{ color: '#fff', fontSize: '15px', marginLeft: '0.2rem' }} />
+          </span>
+        </p>
+      </div>
+      <div className='user-connect-bar'>
+        <p> {user} </p>
+      </div>
     </div>
   );
 };
