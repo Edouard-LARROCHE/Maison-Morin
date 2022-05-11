3bb788faae5cb321f657cd191953dffa5da7eea4
Edouard-LARROCHE <edouard-larroche_student2021@wilder.school>
2022-05-11 12:16:28 +0200

    Maison-Morin

--- utils/errors.utils.js
+++ utils/errors.utils.js
@@ -7,7 +7,8 @@ module.exports.signUpErrors = (err) => {
 
   if (err.message.includes('password')) errors.password = 'Le mot de passe doit faire 6 caractères minimum';
 
-  if (err.code === 11000 && Object.keys(err.keyValue)[0].includes('email')) errors.email = 'Cet email est déjà enregistré';
+  if (err.code === 11000 && Object.keys(err.keyValue)[0].includes('email'))
+    errors.email = 'Cet email est déjà enregistré, veuillez en saisir un autre.';
 
   return errors;
 };
