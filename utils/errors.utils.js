4160d6360adcf1b916dd2d6c25a3531d4351bc8d
Edouard-LARROCHE <edouard-larroche_student2021@wilder.school>
2022-05-11 12:03:28 +0200

    Maison-Morin

--- utils/errors.utils.js
+++ utils/errors.utils.js
@@ -1,7 +1,7 @@
 module.exports.signUpErrors = (err) => {
   let errors = { name: '', email: '', password: '' };
 
-  if (err.message.includes('lastName' || 'firstName')) errors.name = 'Le nom et prénom doivent faire 3 caractères minimum';
+  if (err.message.includes('firstName') || err.message.includes('lastName')) errors.name = 'Le nom et prénom doivent faire 3 caractères minimum';
 
   if (err.message.includes('email')) errors.email = 'Format email incorrect';
 
