24a5ee4c76afc0a677fbfb036ed2dc809c48b09c
Edouard-LARROCHE <edouard-larroche_student2021@wilder.school>
2022-05-11 14:19:46 +0200

    Maison-Morin

--- front/src/pages/gallery/traiteur/TraiteurCharcuterie.jsx
+++ front/src/pages/gallery/traiteur/TraiteurCharcuterie.jsx
@@ -1,21 +1,38 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Footer from '../../../components/home/Footer';
 import NavBar from '../../../components/home/NavBar';
 import HeadBand from '../../../components/HeadBand';
 import MenuDrop from '../../../components/dropDown/MenuDrop';
 import Charcuterie from '../../../components/gallery/traiteur/Charcuterie';
 import TopArrow from '../../../components/TopArrow';
+import UserBar from '../../../components/UserBar';
+import { UidContext } from '../../../AppContext';
 
 const TraiteurCharcuterie = () => {
+  const uid = useContext(UidContext);
   return (
-    <div>
-      <NavBar />
-      <HeadBand />
-      <MenuDrop />
-      <Charcuterie />
-      <Footer />
-      <TopArrow />
-    </div>
+    <>
+      {uid ? (
+        <div>
+          <UserBar />
+          <NavBar />
+          <HeadBand />
+          <MenuDrop />
+          <Charcuterie />
+          <Footer />
+          <TopArrow />
+        </div>
+      ) : (
+        <div>
+          <NavBar />
+          <HeadBand />
+          <MenuDrop />
+          <Charcuterie />
+          <Footer />
+          <TopArrow />
+        </div>
+      )}
+    </>
   );
 };
 
--- front/src/pages/gallery/traiteur/TraiteurPoisson.jsx
+++ front/src/pages/gallery/traiteur/TraiteurPoisson.jsx
@@ -1,21 +1,38 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Footer from '../../../components/home/Footer';
 import NavBar from '../../../components/home/NavBar';
 import HeadBand from '../../../components/HeadBand';
 import MenuDrop from '../../../components/dropDown/MenuDrop';
 import Poissons from '../../../components/gallery/traiteur/Poissons';
 import TopArrow from '../../../components/TopArrow';
+import UserBar from '../../../components/UserBar';
+import { UidContext } from '../../../AppContext';
 
 const TraiteurPoisson = () => {
+  const uid = useContext(UidContext);
   return (
-    <div>
-      <NavBar />
-      <HeadBand />
-      <MenuDrop />
-      <Poissons />
-      <Footer />
-      <TopArrow />
-    </div>
+    <>
+      {uid ? (
+        <div>
+          <UserBar />
+          <NavBar />
+          <HeadBand />
+          <MenuDrop />
+          <Poissons />
+          <Footer />
+          <TopArrow />
+        </div>
+      ) : (
+        <div>
+          <NavBar />
+          <HeadBand />
+          <MenuDrop />
+          <Poissons />
+          <Footer />
+          <TopArrow />
+        </div>
+      )}
+    </>
   );
 };
 
