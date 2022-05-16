c361a757d4491f4925d932fd7a89486bacd3fe03
Edouard-LARROCHE <edouard-larroche_student2021@wilder.school>
2022-05-16 13:49:43 +0200

    Maison-Morin

--- front/src/components/cardModal/AddShopCart.jsx
+++ front/src/components/cardModal/AddShopCart.jsx
@@ -1,11 +1,67 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import axios from 'axios';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import { useDispatch, useSelector } from 'react-redux';
+import { addCard } from '../../actions/user.actions';
+import { UidContext } from '../../AppContext';
 
 const AddShopCart = ({ gallery }) => {
+  const [confirm, setConfirm] = useState('confirm-before');
+  const [add, setAdd] = useState('add-before');
+  const [connect, setConnect] = useState('connect-before');
+  const dispatch = useDispatch();
+  const userData = useSelector((state) => state.userReducer);
+  const uid = useContext(UidContext);
+
+  const addStore = () => {
+    const data = {
+      shopCart: gallery._id,
+    };
+
+    if (uid) {
+      if (!userData.shopCart.includes(gallery._id.toString())) {
+        dispatch(addCard(gallery._id));
+        axios
+          .patch(`/api/user/addCard/${uid}`, data)
+          .then((res) => {
+            setConfirm('confirm');
+            setTimeout(() => {
+              setConfirm('confirm-before');
+            }, 1500);
+          })
+          .catch((err) => console.log(err));
+      } else {
+        setAdd('add');
+        setTimeout(() => {
+          setAdd('add-before');
+        }, 1500);
+      }
+    } else {
+      setConnect('connect');
+      setTimeout(() => {
+        setConnect('connect-before');
+      }, 1500);
+    }
+  };
+
   return (
     <div className='add-shop-cart'>
       <div className='price'>
         <h1> € {gallery.price} </h1>
         <p>Prix à l'unité</p>
+      </div>
+      <div className='add-shopping-cart' onClick={() => addStore()}>
+        <p style={{ marginRight: '0.5rem' }}>AJOUTER AU PANIER</p>
+        <ShoppingCartIcon />
+      </div>
+      <div className={confirm}>
+        <p>BIEN AJOUTER</p>
+      </div>
+      <div className={add}>
+        <p>DÉJÀ AJOUTER</p>
+      </div>
+      <div className={connect}>
+        <p>CONNEXION REQUISE</p>
       </div>
     </div>
   );
