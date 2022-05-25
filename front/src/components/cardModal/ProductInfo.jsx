933dc043c2e651dce125808f2650775e06f7a1b6
Edouard-LARROCHE <edouard-larroche_student2021@wilder.school>
2022-05-25 14:16:48 +0200

    Maison-Morin

--- front/src/components/cardModal/ProductInfo.jsx
+++ front/src/components/cardModal/ProductInfo.jsx
@@ -3,14 +3,27 @@ import React from 'react';
 const ProductInfo = ({ gallery }) => {
   return (
     <div className='list-card'>
-      <h2> INFORMATIONS SUR LE VIN : </h2>
-      <ul>
-        <p>Domaine viticole : {gallery.winery} </p>
-        <p>Cépages : {gallery.grapeVarieties} </p>
-        <p>Région : {gallery.region} </p>
-        <p>Style de vin : {gallery.styleOfWine} </p>
-        <p>Teneur en alcool : {gallery.alcoholContent}% </p>
-      </ul>
+      <h2> CARACTÉRISTIQUES DU VIN : </h2>
+      <div className='wine-info'>
+        <div className='info-left'>
+          <ul>
+            <p>Domaine viticole : </p>
+            <p>Cépages : </p>
+            <p>Région : </p>
+            <p>Style de vin : </p>
+            <p>Teneur en alcool : </p>
+          </ul>
+        </div>
+        <div className='info-right'>
+          <ul>
+            <p>{gallery.winery}</p>
+            <p> {gallery.grapeVarieties}</p>
+            <p> {gallery.region}</p>
+            <p> {gallery.styleOfWine}</p>
+            <p> {gallery.alcoholContent}%</p>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
