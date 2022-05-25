03bb9296fdf7ce3c24128b67af2924e7682fc955
Edouard-LARROCHE <edouard-larroche_student2021@wilder.school>
2022-05-25 14:25:32 +0200

    Maison-Morin

--- front/src/components/cardModal/ProductInfo.jsx
+++ front/src/components/cardModal/ProductInfo.jsx
@@ -8,19 +8,29 @@ const ProductInfo = ({ gallery }) => {
         <div className='info-left'>
           <ul>
             <p>Domaine viticole : </p>
+            <div className='line2' />
             <p>Cépages : </p>
+            <div className='line2' />
             <p>Région : </p>
+            <div className='line2' />
             <p>Style de vin : </p>
+            <div className='line2' />
             <p>Teneur en alcool : </p>
+            <div className='line2' />
           </ul>
         </div>
         <div className='info-right'>
           <ul>
             <p>{gallery.winery}</p>
+            <div className='line2' />
             <p> {gallery.grapeVarieties}</p>
+            <div className='line2' />
             <p> {gallery.region}</p>
+            <div className='line2' />
             <p> {gallery.styleOfWine}</p>
+            <div className='line2' />
             <p> {gallery.alcoholContent}%</p>
+            <div className='line2' />
           </ul>
         </div>
       </div>
