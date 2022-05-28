14ef4ee5cb095598b712c78afc6f6cb13eaf3d0b
Edouard-LARROCHE <edouard-larroche_student2021@wilder.school>
2022-05-28 21:00:15 +0200

    Maison-Morin

--- front/src/components/shopCart/ShopCart.jsx
+++ front/src/components/shopCart/ShopCart.jsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const ShopCart = () => {
+  const [localData, setLocalData] = useState([]);
+
+  useEffect(() => {
+    let cardData = window.localStorage.Vins ? window.localStorage.Vins.split(',') : [];
+    for (let i = 0; i < cardData.length; i++) {
+      setLocalData(cardData);
+    }
+  }, []);
+
   return (
-    <div>
-      <h1>ShopCart</h1>
+    <div className='shopCart'>
+      <h2>
+        Votre panier contient {localData.length} article{localData.length > 1 ? 's' : ''}
+      </h2>
+      <div className='grid-list'>
+        <div className='article-list'>
+          <p>Produits vendus par Maison-Morin</p>
+        </div>
+        <div className='total-list'></div>
+      </div>
     </div>
   );
 };
