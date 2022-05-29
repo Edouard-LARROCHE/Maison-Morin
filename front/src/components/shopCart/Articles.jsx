f172938d63a99ef238772bbae89d157e29dbc410
Edouard-LARROCHE <edouard-larroche_student2021@wilder.school>
2022-05-29 21:43:21 +0200

    Maison-Morin

--- front/src/components/shopCart/Articles.jsx
+++ front/src/components/shopCart/Articles.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import DeleteIcon from '@material-ui/icons/Delete';
 
 const Articles = ({ gallery }) => {
   return (
@@ -10,6 +11,7 @@ const Articles = ({ gallery }) => {
         <div className='article'>
           <p> {gallery.name} </p>
           <p>Bouteille 75cl</p>
+          <DeleteIcon className='delete-icon' />
         </div>
         <div className='price-more-less'></div>
       </div>
