ad92a8be42bd76239caead34e5eefc2e568ba9c9
Edouard-LARROCHE <edouard-larroche_student2021@wilder.school>
2022-05-29 22:17:16 +0200

    Maison-Morin

--- front/src/components/shopCart/Articles.jsx
+++ front/src/components/shopCart/Articles.jsx
@@ -2,6 +2,13 @@ import React from 'react';
 import DeleteIcon from '@material-ui/icons/Delete';
 
 const Articles = ({ gallery }) => {
+  let cardData = window.localStorage.Vins ? window.localStorage.Vins.split(',') : [];
+
+  const deleteItemsStore = () => {
+    cardData.filter((id) => id !== gallery._id);
+    console.log(cardData);
+  };
+
   return (
     <div className='articles'>
       <div className='grid-article'>
@@ -11,7 +18,7 @@ const Articles = ({ gallery }) => {
         <div className='article'>
           <p> {gallery.name} </p>
           <p>Bouteille 75cl</p>
-          <DeleteIcon className='delete-icon' />
+          <DeleteIcon className='delete-icon' onClick={deleteItemsStore} />
         </div>
         <div className='price-more-less'></div>
       </div>
