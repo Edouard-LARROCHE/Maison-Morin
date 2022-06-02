41fa14e355f5d0235a82f7623e0834c5727b8a2a
Edouard-LARROCHE <edouard-larroche_student2021@wilder.school>
2022-06-02 17:00:47 +0200

    button to send

--- front/src/components/shopCart/TotalShopCart.jsx
+++ front/src/components/shopCart/TotalShopCart.jsx
@@ -8,7 +8,20 @@ const TotalShopCart = ({ gallery }) => {
         <h1> {gallery.price} € </h1>
       </div>
       <div className='jesaispas'></div>
-      <div className='send-return'></div>
+
+      <div className='send-return'>
+        <div className='return-product'>
+          <div className='btn-selection' style={{ marginTop: '1rem', width: '15.5rem' }}>
+            <p>CONTINUER MES ACHATS</p>
+          </div>
+        </div>
+        <h3>OU</h3>
+        <div className='buy-product'>
+          <div className='btn-selection'>
+            <p>COMMANDER</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
