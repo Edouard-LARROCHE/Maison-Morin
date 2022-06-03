fe80271be27360b39236c88cd10049c2ecc7c85c
Edouard-LARROCHE <edouard-larroche_student2021@wilder.school>
2022-06-03 16:10:13 +0200

    shop cart

--- front/src/components/shopCart/ShopCart.jsx
+++ front/src/components/shopCart/ShopCart.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Articles from './Articles';
-import { useSelector } from 'react-redux';
 import TotalShopCart from './TotalShopCart';
+import { useSelector } from 'react-redux';
 
 const ShopCart = () => {
   const [localData, setLocalData] = useState([]);
