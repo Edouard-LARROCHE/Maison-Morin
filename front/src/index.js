459d851ff312ef880f8f936e0f97167f778e54ab
Edouard-LARROCHE <edouard-larroche_student2021@wilder.school>
2022-06-09 11:08:29 +0200

    _

--- front/src/index.js
+++ front/src/index.js
@@ -11,10 +11,10 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers/index';
-// DELETE BEFORE PROD !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-import { composeWithDevTools } from 'redux-devtools-extension';
+// DELETE BEFORE PROD
+// import { composeWithDevTools } from 'redux-devtools-extension';
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <Provider store={store}>
